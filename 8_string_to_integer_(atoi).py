e4284124990f00d399cff5dc3a875f58c50f3711
agp22888 <agp22888@gmail.com>
2024-01-26 14:44:35 +0300

    problem #8 match case version

--- 8_string_to_integer_(atoi).py
+++ 8_string_to_integer_(atoi).py
@@ -1,34 +1,31 @@
-import re
-import string
-
-
 class Solution:
     int_max = 2147483647
     int_min = -2147483648
-    char_dict = {
-        "0": 0,
-        "1": 1,
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9,
 
-    }
-
-    def myAtoi_(self, s: str) -> int:
-        match = re.search(r'^ *([+|-]?[0123456789]+)', s)
-        if not match:
-            return 0
-        result = int(match.group())
-        if result > 0:
-            result = min(result, self.int_max)
-        else:
-            result = max(result, self.int_min)
-        return result
+    @staticmethod
+    def my_int(c: str) -> int | None:
+        match c:
+            case "0":
+                return 0
+            case "1":
+                return 1
+            case "2":
+                return 2
+            case "3":
+                return 3
+            case "4":
+                return 4
+            case "5":
+                return 5
+            case "6":
+                return 6
+            case "7":
+                return 7
+            case "8":
+                return 8
+            case "9":
+                return 9
+        return None
 
     def myAtoi(self, s: str) -> int:
         s = s.strip()
@@ -41,10 +38,9 @@ class Solution:
             positive = s[0] == '+'
         result = 0
         for ch in s[start:]:
-            try:
-                result = result * 10 + self.char_dict[ch]
-            except KeyError:
+            if (converted := self.my_int(ch)) is None:
                 break
+            result = result * 10 + converted
         result = (1 if positive else -1) * result
         if positive:
             result = min(result, self.int_max)
